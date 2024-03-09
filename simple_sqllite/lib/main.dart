057766f9474d913e5d6b6373bd378834da7f278b
Tona-Mangobyte <tona@mango-byte.com>
2024-03-09 14:57:57 +0700

    init make simple local db storage

---
+++ simple_sqllite/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:simple_sqllite/screens/home_screen.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Future<void> initState() async {
+    super.initState();
+    // Get a location using getDatabasesPath
+    var databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'demo.db');
+
+// Delete the database
+    await deleteDatabase(path);
+
+// open the database
+    Database database = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+          // When creating the db, create the table
+          await db.execute(
+              'CREATE TABLE Test (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)');
+        });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple SQLite',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
