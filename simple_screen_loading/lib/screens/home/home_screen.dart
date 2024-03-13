9399601e7c5f7a59d262940ffa2d41f818d2073d
Tona-Mangobyte <tona@mango-byte.com>
2024-03-13 15:30:01 +0700

    init simple loading on screen

---
+++ simple_screen_loading/lib/screens/home/home_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:simple_screen_loading/screens/home/widgets/card_list_item.dart';
+import 'package:simple_screen_loading/screens/home/widgets/circle_list_item.dart';
+import 'package:simple_screen_loading/screens/home/widgets/shimmer.dart';
+import 'package:simple_screen_loading/screens/home/widgets/shimmer_loading.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+const _shimmerGradient = LinearGradient(
+  colors: [
+    Color(0xFFEBEBF4),
+    Color(0xFFF4F4F4),
+    Color(0xFFEBEBF4),
+  ],
+  stops: [
+    0.1,
+    0.3,
+    0.4,
+  ],
+  begin: Alignment(-1.0, -0.3),
+  end: Alignment(1.0, 0.3),
+  tileMode: TileMode.clamp,
+);
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isLoading = true;
+
+  void _toggleLoading() {
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Shimmer(
+        linearGradient: _shimmerGradient,
+        child: ListView(
+          physics: _isLoading ? const NeverScrollableScrollPhysics() : null,
+          children: [
+            const SizedBox(height: 16),
+            _buildTopRowList(),
+            const SizedBox(height: 16),
+            _buildListItem(),
+            _buildListItem(),
+            _buildListItem(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleLoading,
+        child: Icon(
+          _isLoading ? Icons.hourglass_full : Icons.hourglass_bottom,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTopRowList() {
+    return SizedBox(
+      height: 72,
+      child: ListView(
+        physics: _isLoading ? const NeverScrollableScrollPhysics() : null,
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        children: [
+          const SizedBox(width: 16),
+          _buildTopRowItem(),
+          _buildTopRowItem(),
+          _buildTopRowItem(),
+          _buildTopRowItem(),
+          _buildTopRowItem(),
+          _buildTopRowItem(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTopRowItem() {
+    return ShimmerLoading(
+      isLoading: _isLoading,
+      child: const CircleListItem(),
+    );
+  }
+
+  Widget _buildListItem() {
+    return ShimmerLoading(
+      isLoading: _isLoading,
+      child: CardListItem(
+        isLoading: _isLoading,
+      ),
+    );
+  }
+}
