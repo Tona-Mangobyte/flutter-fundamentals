f9e243cfdfe0a869ad9cef4780d6b93f4dd4f2c3
Tona-Mangobyte <tona@mango-byte.com>
2024-03-14 13:14:12 +0700

    make simple menu animate

--- simple_animations/lib/main.dart
+++ simple_animations/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simple_animations/screens/home_screen.dart';
 import 'package:simple_animations/screens/list_screen.dart';
+import 'package:simple_animations/screens/menu_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,8 +19,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      // home: const HomeScreen(),
       // home: const ListScreen(),
+      home: const MenuScreen(),
     );
   }
 }
